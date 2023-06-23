d8a38fe7a9bf063fa1727f8d7f1dde3172e99933
Cristina Leonor Algañaraz Rosado <134443391+CristinaAlganaraz@users.noreply.github.com>
2023-06-23 20:58:56 -0300

    Add files via upload

---
+++ Guia-08-POO/ActividadExtra01/Entidad/Vehiculo.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+/**
+ *
+ * @author crist
+ */
+public class Vehiculo {
+
+    private String tipo;
+    private String marca;
+    private String modelo;
+    private int anio;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String tipo, String marca, String modelo, int anio) {
+        this.tipo = tipo;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+}
