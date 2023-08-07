2caa92d041487de7b8576c9b4342c3a73204be43
Cristina Leonor Algañaraz Rosado <134443391+CristinaAlganaraz@users.noreply.github.com>
2023-08-07 19:19:23 -0300

    Add files via upload

---
+++ Guia-10-Colecciones/EjercicioExtra03/Entidad/Libro.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author crist
+ */
+public class Libro {
+
+    private String titulo;
+    private String autor;
+    private int numeroEjemplares;
+    private int numeroEjemplaresPrestados;
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String autor, int numeroEjemplares) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroEjemplares = numeroEjemplares;
+    }
+
+    public Libro(String titulo, String autor, int numeroEjemplares, int numeroEjemplaresPrestados) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroEjemplares = numeroEjemplares;
+        this.numeroEjemplaresPrestados = numeroEjemplaresPrestados;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroEjemplares() {
+        return numeroEjemplares;
+    }
+
+    public void setNumeroEjemplares(int numeroEjemplares) {
+        this.numeroEjemplares = numeroEjemplares;
+    }
+
+    public int getNumeroEjemplaresPrestados() {
+        return numeroEjemplaresPrestados;
+    }
+
+    public void setNumeroEjemplaresPrestados(int numeroEjemplaresPrestados) {
+        this.numeroEjemplaresPrestados = numeroEjemplaresPrestados;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.titulo);
+        hash = 37 * hash + Objects.hashCode(this.autor);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Libro other = (Libro) obj;
+        if (!Objects.equals(this.titulo, other.titulo)) {
+            return false;
+        }
+        if (!Objects.equals(this.autor, other.autor)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Título: " + titulo + ", Autor: " + autor + ", Cantidad de ejemplares: " + numeroEjemplares + ", Cantidad de ejemplares prestados: " + numeroEjemplaresPrestados;
+    }
+}
